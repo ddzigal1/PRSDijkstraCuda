aed872946ba7bf8117b8bd963046ee3967f69c50
Džemil Džigal <jemo@localhost.localdomain>
2019-12-10 22:54:27 +0100

    uklanjanje bespotrebnog koda

--- algoritam.py
+++ algoritam.py
@@ -1,46 +1,6 @@
 import cv2
 import numpy as np
 import math
-
-
-# class ShapeDetector:
-#     def __init__(self):
-#         pass
-
-#     def detect(self, c):
-#         # initialize the shape name and approximate the contour
-#         shape = "unidentified"
-#         peri = cv2.arcLength(c, True)
-#         approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-#         # if the shape is a triangle, it will have 3 vertices
-#         if len(approx) == 3:
-#             shape = "triangle"
-
-#         # if the shape has 4 vertices, it is either a square or
-#         # a rectangle
-#         elif len(approx) == 4:
-#             # compute the bounding box of the contour and use the
-#             # bounding box to compute the aspect ratio
-#             (x, y, w, h) = cv2.boundingRect(approx)
-#             ar = w / float(h)
-
-#             # a square will have an aspect ratio that is approximately
-#             # equal to one, otherwise, the shape is a rectangle
-#             shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
-
-#         # if the shape is a pentagon, it will have 5 vertices
-#         elif len(approx) == 5:
-#             shape = "pentagon"
-
-#         # otherwise, we assume the shape is a circle
-#         else:
-#             shape = "circle"
-
-#         # return the name of the shape
-#         return shape
-
-
-# sd = ShapeDetector()
 face_cascade = cv2.CascadeClassifier('face2.xml')
 video = cv2.VideoCapture(0)
 hueLower=3
@@ -110,7 +70,7 @@ while video.isOpened():
 
     cv2.drawContours(img2, contours, -1, (255, 255, 0), 3)
 
-    cv2.imshow('hand', img3)
+    cv2.imshow('kamen_papir_makaze', img3)
 
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
